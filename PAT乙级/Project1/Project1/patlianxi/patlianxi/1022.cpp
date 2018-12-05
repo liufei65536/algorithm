dc1e3b7f0c2d46b5c9d69469293f55deb3a5a936
FF <1541569284@qq.com>
2018-12-05 16:51:48 +0800

    1022进制转换

---
+++ PAT乙级/Project1/Project1/patlianxi/patlianxi/1022.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+void toD(int S, int D);
+int main()
+{
+	int A, B,D;
+	cin >> A >> B>>D;
+	int S = A + B;
+	toD(S, D);
+	return 0;
+}
+
+/*
+把十进制转换成D进制？
+12-->2进制
+2^3+2^2
+1100
+*/
+void toD(int S,int D) {
+	int maxP=0;
+	for (int i = 0; i < 31; i++) {
+		if (pow(D, i) <= S && pow(D,i+1)>S) {
+			maxP = i;
+			break;
+		}
+	}
+	int *a = new int[maxP+1];
+	for (int i = 0; i <= maxP; i++)
+		a[i] = 0;
+	int yu = S;
+	for (int i = maxP; i >= 0; i--) {
+		if (yu > 0) {
+			for (int j = 0; j < D; j++) {
+				if ((yu - j * pow(D, i) >= 0 && yu - (j + 1)* pow(D, i) < 0)) {
+					a[i] = j;
+					yu -= j * pow(D, i);
+				}
+			}
+		}
+	}
+	for (int i = maxP; i >= 0; i--) {
+		cout << a[i];
+	}
+}
